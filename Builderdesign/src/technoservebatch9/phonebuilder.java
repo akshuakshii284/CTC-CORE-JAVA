98441565f5f9bf51a25ef1fb1b47d9b71f7b9df3
akshuakshii284 <akshuakshii284@gmail.com>
2022-02-21 23:07:16 +0530

    third commit

---
+++ Builderdesign/src/technoservebatch9/phonebuilder.java
@@ -0,0 +1,39 @@
+package technoservebatch9;
+
+public class phonebuilder {
+	private String os;
+	private int ram;
+	private String processor;
+	private double screensize;
+	private int battery;
+	public phonebuilder setOs(String os) {
+		this.os = os;
+		return this;
+	}
+	public phonebuilder setRam(int ram) {
+		this.ram = ram;
+		return this;
+	}
+	public phonebuilder setProcessor(String processor) {
+		this.processor = processor;
+		return this;
+	}
+	public phonebuilder setScreensize(double screensize) {
+		this.screensize = screensize;
+		return this;
+	}
+	public phonebuilder setBattery(int battery) {
+		this.battery = battery;
+		return this;
+	}
+
+	
+	public Phone getPhone() {
+		return new Phone(os,ram,processor,screensize,battery);
+	}
+	
+	
+	
+	
+	
+}
---
+++ Builderdesign/src/technoservebatch9/shop.java
@@ -0,0 +1,11 @@
+package technoservebatch9;
+
+public class shop {
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		Phone p=new phonebuilder().setOs("Android").setRam(4).getPhone();
+		System.out.println(p);
+
+	}
+
+}
