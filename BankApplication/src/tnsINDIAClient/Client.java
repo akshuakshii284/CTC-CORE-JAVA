087a1c489697ddf29f0e35632f48127c79c7462e
akshuakshii284 <akshuakshii284@gmail.com>
2022-03-01 18:19:25 +0530

    5th commit

--- BankApplication/src/tnsINDIAClient/Client.java
+++ BankApplication/src/tnsINDIAClient/Client.java
@@ -13,9 +13,10 @@ public class Client {
 		BankFactory bf=new MMBankFactory();
 		SavingAcc sa=new MMSavingAcc(123456,"rani",50000);
 		sa.withdraw(45000);
+		sa.deposite(4000);
 		CurrentAcc ca=new MMCurrentAcc(45678,"rammu",5000);
 		ca.withdraw(2000);
-
+         ca.deposite(200);
 	}
 
 }
